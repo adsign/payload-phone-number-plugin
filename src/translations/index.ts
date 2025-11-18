0d551b5d74a8bd19cbe50b4a9901ae1224bb7d80
Sondre Ørland <post@sondreorland.no>
2025-11-18 15:40:43 +0100

    feat: add swedish translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,10 +2,12 @@ import type { GenericTranslationsObject, NestedKeysStripped } from '@payloadcms/
 
 import { en } from './en.js';
 import { nb } from './nb.js';
+import { sv } from './sv.js';
 
 export const translations = {
     en,
     nb,
+    sv,
 };
 
 export type PayloadPhoneNumberPluginTranslations = GenericTranslationsObject;
