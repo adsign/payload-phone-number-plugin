908e3e29360e7664c670783cd63bd0ceb8ad24cd
Sondre Ørland <78776152+sondreorland@users.noreply.github.com>
2026-01-15 13:03:36 +0100

    chore(deps): replace google-libphonenumber with libphonenumber-js (#21)

--- src/utilities/getPhoneNumberDetails.ts
+++ src/utilities/getPhoneNumberDetails.ts
@@ -1,31 +1,25 @@
 import type { PhoneNumber } from '../types.js';
 
-import libphonenumber from 'google-libphonenumber';
-const { PhoneNumberUtil, PhoneNumberFormat } = libphonenumber;
+import { parsePhoneNumber } from 'libphonenumber-js/max';
 
 import { countries } from './countries.js';
 
-let phoneUtil: libphonenumber.PhoneNumberUtil | null = null;
-
 export function getPhoneNumberDetails(e164PhoneNumber: string): PhoneNumber | null {
-    if (!phoneUtil) {
-        phoneUtil = PhoneNumberUtil.getInstance();
-    }
-
     try {
-        const number = phoneUtil.parse(e164PhoneNumber);
-        const regionCode = phoneUtil.getRegionCodeForNumber(number);
+        const phoneNumber = parsePhoneNumber(e164PhoneNumber);
+
+        const regionCode = phoneNumber.country;
 
         if (!regionCode) {
             return null;
         }
 
-        const national = phoneUtil.format(number, PhoneNumberFormat.NATIONAL);
-        const international = phoneUtil.format(number, PhoneNumberFormat.INTERNATIONAL);
-        const e164Formatted = phoneUtil.format(number, PhoneNumberFormat.E164);
+        const national = phoneNumber.formatNational();
+        const international = phoneNumber.formatInternational();
+        const e164Formatted = phoneNumber.number;
 
         const country = countries.find((c) => c.regionCode === regionCode);
-        const callingCode = country?.callingCode || `+${number.getCountryCode()}`;
+        const callingCode = country?.callingCode || `+${phoneNumber.countryCallingCode}`;
 
         return {
             e164: e164Formatted,
