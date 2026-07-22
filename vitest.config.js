ac1792857eb831db509d245ce415689683c1855a
Sondre Ørland <post@sondreorland.no>
2026-07-22 02:27:46 +0200

    chore(deps): use vitest native tsconfig path resolution

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { loadEnv } from 'payload/node';
 import { fileURLToPath } from 'url';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 const filename = fileURLToPath(import.meta.url);
@@ -11,11 +10,9 @@ export default defineConfig(() => {
     loadEnv(path.resolve(dirname, './dev'));
 
     return {
-        plugins: [
-            tsconfigPaths({
-                ignoreConfigErrors: true,
-            }),
-        ],
+        resolve: {
+            tsconfigPaths: true,
+        },
         test: {
             environment: 'node',
             hookTimeout: 30_000,
