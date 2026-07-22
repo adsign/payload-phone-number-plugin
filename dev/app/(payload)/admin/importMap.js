a813d1fb965b594d07e7dfd49be8d44e470322ab
Sondre Ørland <78776152+sondreorland@users.noreply.github.com>
2026-07-22 02:22:40 +0200

    chore(deps): upgrade dependencies (#48)

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,7 +1,10 @@
 import { PhoneNumberCellComponent as PhoneNumberCellComponent_acc6f3dfddf27a115c1f737a3b7bbd60 } from 'payload-phone-number-plugin/client'
 import { PhoneNumberFieldComponent as PhoneNumberFieldComponent_acc6f3dfddf27a115c1f737a3b7bbd60 } from 'payload-phone-number-plugin/client'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
   "payload-phone-number-plugin/client#PhoneNumberCellComponent": PhoneNumberCellComponent_acc6f3dfddf27a115c1f737a3b7bbd60,
-  "payload-phone-number-plugin/client#PhoneNumberFieldComponent": PhoneNumberFieldComponent_acc6f3dfddf27a115c1f737a3b7bbd60
+  "payload-phone-number-plugin/client#PhoneNumberFieldComponent": PhoneNumberFieldComponent_acc6f3dfddf27a115c1f737a3b7bbd60,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
